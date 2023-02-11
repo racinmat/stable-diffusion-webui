d32436deeb709d850e03836478589014b34e6a03
Matěj Račinský <racinsky.matej@seznam.cz>
2023-02-11 15:16:03 +0100

    added note added sample config and moved everything to the extension some smaller values for ntb, deleted unused file moved the js to the extension code moved more code form the main codebase to the extension updated the condigs more unification with the dev branch hiding external links using flags removed comment, returned functionality for buttons updated config pruned blacklist to remove redundant phrases added more nude-producing phrases to blacklist adjusted negative prompt a bit, added more nude words there added prompt analysis added more functionality to param tuning fix after refactoring added fix based on the dev comment added note about tuning public changes to blacklist moved scripts to script dir and fixed missing imports added new option for custom css, cloned batch processing removed spaces at end Update ui-config.json fixed blacklist, removed best shadow returned footer, but removed all links. All texts are 2 times larger added mousketool disabled image listing disabled footer and frozen settings adjusted blacklist and negative prompt based on tryouts at place fixed defender url set correct url logging exact match of blacklist added note about full config removed some duplicates added the functionality for QR code, new row for output renamed bat file added bat files moved my config stuff to separate json final hiding setting label for even more buttons, setting them to invisible removed debug print removing the print added git submodule removed logs pushed extension pushed extension removed some prints and extensions from gitignore hidden the buttons and yet another tab added ui config default, added config af with hiding tabs, adding it as cli arg added my notes and lots of magnet links added my notes

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,26 @@
 @echo off
 
-set PYTHON=
+::set PYTHON=C:\ProgramData\Anaconda3\envs\image_generation\python.exe
+:: it must be python 3.10 to work properly
+::set PYTHON=C:\Users\Animefest\miniconda3\python.exe
+set PYTHON=C:\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+:: the hide-ui-dir-config is separate flag, hides server-side
+set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config-af.json --ui-config-file=ui-config-af.json --hide-ui-dir-config --freeze-settings
+:: share to local network by adding --share --listen
+::set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config-af.json --ui-config-file=ui-config-af.json --hide-ui-dir-config --freeze-settings --share --listen
+:: same settings for some stuff, but unlocked UI
+::set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config-af.json --ui-config-file=ui-config-default.json --hide-ui-dir-config --freeze-settings --share --listen
+::unlocked version of AF, for local network and allowing to install stuff, very dangerous, do not run publicly
+::set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config-af-unlocked.json --ui-config-file=ui-config-af-unlocked.json --hide-ui-dir-config --share --listen --enable-insecure-extension-access
+:: for my laptop
+::set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config-af.json --hide-ui-dir-config --ui-config-file=ui-config-ntb.json
+:: for the full-config in my fork
+::set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config.json --ui-config-file=ui-config-default.json
+::set COMMANDLINE_ARGS=--theme dark --medvram
+::set COMMANDLINE_ARGS=--theme dark --lowvram
+::set COMMANDLINE_ARGS=--theme dark --use-cpu all --no-half
+
 
 call webui.bat
